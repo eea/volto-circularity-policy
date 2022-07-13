192b5a646d1709dac695a9d7687b73ee2d57f48a
Alexandru Ghica <alec.ghica@eaudeweb.ro>
2022-07-13 15:04:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
+import { runtimeConfig } from '@plone/volto/runtime_config';
+
 const applyConfig = (config) => {
+
+  // #137187 Keycloak integration
+  if (runtimeConfig['RAZZLE_KEYCLOAK'] === 'Yes') {
+    config.settings.externalRoutes = [
+      ...(config.settings.externalRoutes || []),
+      {
+        match: {
+          path: '/login',
+          exact: true,
+          strict: false,
+        },
+      },
+      {
+        match: {
+          path: '/logout',
+          exact: true,
+          strict: false,
+        },
+      },
+    ];
+  }
+
   return config;
 };
 
