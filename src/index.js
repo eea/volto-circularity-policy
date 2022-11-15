72ab424bbd0186d0fd88dddc476582ce26a16d3b
nileshgulia1 <nileshgulia@gmail.com>
2022-11-15 19:04:32 +0530

    add(bodyclass): block for layouts in plone site root

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import * as eea from '@eeacms/volto-eea-website-theme/config';
 import { runtimeConfig } from '@plone/volto/runtime_config';
 import StagingBanner from './StagingBanner';
+import {
+  BodyClassEdit,
+  BodyClassView,
+} from '@eeacms/volto-circularity-policy/components/manage/Blocks/BodyClass';
+import worldSVG from '@plone/volto/icons/world.svg';
 import './less/slate-metadata.less';
 
 const applyConfig = (config) => {
@@ -52,6 +57,37 @@ const applyConfig = (config) => {
       buttonTitle: 'Go to full site search',
     },
   ];
+
+  config.settings.showTags = false;
+
+  // Enable Title block
+  config.blocks.blocksConfig.title.restricted = false;
+
+  // Enable description block (also for cypress)
+  config.blocks.blocksConfig.description.restricted = false;
+
+  //Make all blocks deleteable
+  config.blocks.requiredBlocks = [];
+
+  config.settings.isMultilingual = false;
+
+  config.blocks.blocksConfig.body_classname = {
+    id: 'body_classname',
+    title: 'Body classname',
+    icon: worldSVG,
+    group: 'common',
+    edit: BodyClassEdit,
+    view: BodyClassView,
+    restricted: false,
+    mostUsed: false,
+    sidebarTab: 1,
+    blocks: {},
+    security: {
+      addPermission: [],
+      view: [],
+    },
+    blockHasOwnFocusManagement: true,
+  };
 
   return config;
 };
